ddec9367d687cb6849cf5315d790de36c020e9ef
Aaron Saray <me@aaronsaray.com>
2023-12-03 12:58:45 -0600

    Progress on removing the history

--- src/background.js
+++ src/background.js
@@ -1,10 +1,38 @@
-const patterns = [
-    'https://yahoo.com',
-    'https://slashdot.org',
+/// <reference path="../node_modules/chrome-types/_all.d.ts"/>
+
+const domains = [
+    'www.yahoo.com',
+    'slashdot.org',
 ];
 
+function makeRegexFromDomain(domain) {
+    // remove www if it exists
+    let parsed = domain.replace(/^www\./, '');
+
+    // add on optional www
+    parsed = '(www\.)?' + parsed;
+
+    // add optional http(s)
+    parsed = 'http(s)?://' + parsed;
+
+    // add optional anything trailing
+    parsed = parsed + '/.*';
+
+    // add modifiers
+    parsed = '^' + parsed + '$';
+
+    return parsed;
+}
+
+/**
+ * @param {chrome.history.HistoryItem} historyItem 
+ */
 function onVisitedHandler(historyItem) {
-    console.log('history item', historyItem);
+    if (historyItem.url) {
+        if (domains.find(domain => new RegExp(makeRegexFromDomain(domain)).test(historyItem.url))) {
+            chrome.history.deleteUrl({ url: historyItem.url });
+        }
+    }
 }
 
 chrome.history.onVisited.addListener(onVisitedHandler);
